60b8279f9eddffa78f45b89f76b0f43025ce77ea
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-09-13 21:01:51 -0700

    Write the rounding code

--- src/rounder.cpp
+++ src/rounder.cpp
@@ -1,3 +1,38 @@
 #include "rounder.h"
 
+#include <cmath>
+
 Rounder::Rounder() {}
+
+int Rounder::GetDecimalPlaces(double number)
+{
+    std::string numberStr = std::to_string(number);
+    size_t decimalPos = numberStr.find('.');
+    if (decimalPos == std::string::npos) {
+        return 0; // No decimal point found
+    }
+    return numberStr.length() - decimalPos - 1;
+}
+
+double Rounder::roundtoDecimalPlaces(double number, int decimalPlaces)
+{
+    double factor = std::pow(10.0, decimalPlaces);
+    return static_cast<int>(std::round(number * factor) / factor);
+}
+
+int Rounder::RoundToNearestInteger(double number)
+{
+    return static_cast<int>(std::round(number));
+}
+
+int Rounder::GetHighestDecimalPlaces(std::vector<double> numbers)
+{
+    int maxDecimalPlaces = 0;
+    for (double number : numbers) {
+        int decimalPlaces = GetDecimalPlaces(number);
+        if (decimalPlaces > maxDecimalPlaces) {
+            maxDecimalPlaces = decimalPlaces;
+        }
+    }
+    return maxDecimalPlaces;
+}
