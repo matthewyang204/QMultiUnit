4290e6b56a13fbc53e5f5a0de8975b056a8b9f30
Matthew Yang <matthew.yang204@gmail.com>
2026-09-14 11:50:09 -0700

    Write the additional conversions for airflow and temperature

---
+++ src/additionalconv.cpp
@@ -0,0 +1,96 @@
+#include "additionalconv.h"
+#include "mainconv.h"
+
+#include <stdexcept>
+
+AdditionalConv::AdditionalConv(ConversionDicts& mainconversiondicts) {
+    primaryconversiondicts = mainconversiondicts;
+}
+
+double AdditionalConv::TempConvert(QString fromUnit, QString toUnit, double userInput) {
+    double calcTemp;
+
+    if (fromUnit == "C" && toUnit == "F"){
+        calcTemp = (userInput * (9.0/5.0)) + 32.0;
+        return calcTemp;
+    } else if (fromUnit == "C" && toUnit == "K"){
+        calcTemp = userInput + 273.15;
+        return calcTemp;
+    } else if (fromUnit == "F" && toUnit == "C"){
+        calcTemp = (userInput - 32.0) * 5.0 / 9.0;
+        return calcTemp;
+    } else if (fromUnit == "F" && toUnit == "K"){
+        calcTemp = (userInput - 32.0) * 5.0 / 9.0 + 273.15;
+        return calcTemp;
+    } else if (fromUnit == "K" && toUnit == "C"){
+        calcTemp = userInput - 273.15;
+        return calcTemp;
+    } else if (fromUnit == "K" && toUnit == "F"){
+        calcTemp = (userInput - 273.15) * 9.0 / 5.0 + 32.0;
+        return calcTemp;
+    } else if (fromUnit == toUnit){
+        return userInput;
+    }
+
+    throw std::invalid_argument("Invalid Temperature conversion.");
+}
+
+double AdditionalConv::AFConvert(QString fromUnit, QString toUnit, double userInput, QString areaUnit, double areaWidth, double areaHeight){
+    double calcTemp;
+    double area;
+
+    if (fromUnit == "CFM" || toUnit == "CFM") {
+        double width = areaWidth *
+                       primaryconversiondicts.LengthRatios.value(areaUnit) /
+                       primaryconversiondicts.LengthRatios.value("Feet");
+
+        double height = areaHeight *
+                        primaryconversiondicts.LengthRatios.value(areaUnit) /
+                        primaryconversiondicts.LengthRatios.value("Feet");
+
+        area = width * height;
+
+        if (area <= 0) {
+            throw std::invalid_argument(
+                "Your area is 0 or negative. This will give you meaningless results."
+                );
+        }
+    }
+
+    if (fromUnit == "CFM" && toUnit == "LFM") {
+        calcTemp = userInput / area;
+        return calcTemp;
+
+    } else if (fromUnit == "LFM" && toUnit == "CFM") {
+        calcTemp = userInput * area;
+        return calcTemp;
+
+    } else if (fromUnit == toUnit) {
+        return userInput;
+
+    } else if (primaryconversiondicts.SpeedRatios.contains(fromUnit) ||
+               primaryconversiondicts.SpeedRatios.contains(toUnit)) {
+
+        if (fromUnit == "CFM") {
+            userInput = userInput / area;
+            fromUnit = "LFM";
+
+        } else if (toUnit == "CFM") {
+            calcTemp =
+                userInput *
+                primaryconversiondicts.SpeedRatios.value(fromUnit) /
+                primaryconversiondicts.SpeedRatios.value("LFM");
+
+            return calcTemp * area;
+        }
+
+        calcTemp =
+            userInput *
+            primaryconversiondicts.SpeedRatios.value(fromUnit) /
+            primaryconversiondicts.SpeedRatios.value(toUnit);
+
+        return calcTemp;
+    }
+
+    throw std::invalid_argument("Invalid Airflow conversion.");
+}
---
+++ src/additionalconv.h
@@ -0,0 +1,18 @@
+#ifndef ADDITIONALCONV_H
+#define ADDITIONALCONV_H
+
+#include "mainconv.h"
+
+#include <QString>
+
+class AdditionalConv
+{
+public:
+    AdditionalConv(ConversionDicts& mainconversiondicts);
+    double TempConvert(QString fromUnit, QString toUnit, double userInput);
+    double AFConvert(QString fromUnit, QString toUnit, double userInput, QString areaUnit, double areaWidth, double areaHeight);
+private:
+    ConversionDicts primaryconversiondicts;
+};
+
+#endif // ADDITIONALCONV_H
