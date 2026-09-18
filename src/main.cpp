80a05848a6928702965f94e00ee88d6ac2f0363d
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-09-18 12:34:21 -0700

    Set DPI properly on Qt 5

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@
 
 int main(int argc, char *argv[])
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    qDebug() << "I: Setting Retina-grade DPI on Qt 5";
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
     QApplication a(argc, argv);
     a.setStyle(QStyleFactory::create("Fusion"));
 
